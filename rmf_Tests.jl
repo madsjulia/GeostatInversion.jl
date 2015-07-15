ff9cb057a5c06f79da7987665e8276ee082c8aac
Ellen Le <ele@lanl.gov>
2015-07-15 16:55:06 -0600

    randomized k-svd on Q from 2D example, test that is 6 or 7 times faster than truncated k-svd

---
+++ rmf_Tests.jl
@@ -0,0 +1,121 @@
+# Ellen Le June 8-July 14, 2015
+# implements some random matrix factorization techniques
+# Ref: rmf.jl in randommatrixfactorization repo and Halko paper
+using HDF5, JLD
+SAVEDQ_FLAG = 2
+
+# Algo 4.1 Range Finder - finds an orthonormal basis with l columns to
+# approx. range of A - p 240
+# Use for when you want to specify the rank of the decomposition. For
+# specifying the accuracy, use Algo 4.2 instead
+# If you want a rank k, choose l = k + p where p = 5,10,15
+
+function rangefinder1(A,l)
+    srand(1)
+    m = size(A, 1)
+    n = size(A, 2)
+    Omega = randn(n, l) #Gaussian requires less oversampling but is more
+    #costly to construct, see sect 4.6 Halko
+    Y = A*Omega
+    Q,R = qr(Y)
+    return Q
+end
+
+srand(1)
+#Creates a random nxn input matrix A of rank m. Tests Algo 4.1 with oversampling p=10.
+function test_rangefinder(n, m)
+    A = Array(Float64, (n, n))
+    range = randn(n, m)
+    for i = 1:n
+	sum = zeros(n)
+	for j = 1:m
+	    sum += randn() * range[:, j]
+	end
+	A[:, i] = sum
+    end
+    tic()
+    Q1 = rangefinder1(A,m+10)
+    saveTime = toq()
+    println("4.1: Rank of A is $(m) and should be less than or equal to the no. of columns of Q which is $(size(Q1, 2))")
+    println("4.1: Stage A error $(norm(A - Q1 * ctranspose(Q1) * A)) should be less than desired tolerance")
+    println("4.1: time for this algorithm is $(saveTime) seconds")
+end
+
+
+# See what rank we need
+lenCoords = 840
+
+#Load the Q from the 2D problem
+if SAVEDQ_FLAG == 1
+    Q = load("pcga.jl/ellenQ.jld","Q");
+    Z = load("pcga.jl/ellenQ.jld","Z");
+elseif SAVEDQ_FLAG == 2
+    include(abspath("./pcga.jl/ellen.jl"))
+else
+    println("check Q")
+end
+@show(rank(Q))
+
+p = 20 #change p here and in the line below
+function randSVDzetas(A,K; p = 20)
+    Q = rangefinder1(A,K+p);
+    B = Q' * A;      # 
+    U,S,V = svd(B); #This is algorithm 5.1, Direct SVD
+    Sh = diagm(sqrt([S[1:K];zeros(p)])) #Oversample by p
+    Z = V*Sh 
+    return Z
+end  
+
+ranks = [50:25:300]
+n_ranks = length(ranks)
+
+relerr_a = Array(Float64,n_ranks)
+relerr_b = Array(Float64,n_ranks)
+
+# The best rank-100 approximation (deterministic), save this SVD for plot
+lenQ = size(Q,1)
+tic()
+U,S,V = svd(Q)
+Sh = sqrt(S)
+Z1 = V*diagm([Sh[1:100];zeros(lenQ-100)])
+time_bestrankK = toq()
+
+# Plot error comparison of randomized rank-K versus best rank-K
+for i = 1:n_ranks
+    K = ranks[i]
+    Za = randSVDzetas(Q,K)
+    relerr_a[i] = norm(Q-Za*Za')/norm(Q)
+
+    Sh = sqrt(S)
+    Zb = V*diagm([Sh[1:K];zeros(lenQ-K)])
+    relerr_b[i] = norm(Q-Zb*Zb')/norm(Q)
+end
+
+using PyPlot
+figure()
+plot(ranks,relerr_a,ranks,relerr_b,marker="o")
+xlabel("RANK (of basis for approximate range of Q and approx rank of decomposition)")
+ylabel("RELATIVE ERROR ||Q-Z*Z'||/||Q||")
+title("Rank of approximation versus error with p=$(p)")
+legend(["approximation error with oversampling parameter p=$(p) ", "true relative error of best rank k
+        approx"],loc = "best")
+
+tic()
+Z2 = randSVDzetas(Q,100)
+time_approxrankK = toq()
+
+relerror_bestrankK = norm(Q-Z1*Z1')/norm(Q)
+relerror_approxrankK = norm(Q-Z2*Z2')/norm(Q)
+
+@show(p)
+@show(rank(Z1))
+@show(relerror_bestrankK)
+@show(time_bestrankK)
+
+@show(rank(Z2))
+@show(relerror_approxrankK)
+@show(time_approxrankK)
+
+timespeedup = time_bestrankK/time_approxrankK
+@show(timespeedup)
+
