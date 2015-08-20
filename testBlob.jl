4344221e385f163728b82b5141d6cdd0271d4797
Ellen Le <ele@lanl.gov>
2015-08-20 12:18:01 -0600

    runs blob.jl, black and white test

---
+++ testBlob.jl
@@ -0,0 +1,178 @@
+# Runs blobV2.jl
+
+ # Last updated 20 August 2015 by Ellen Le
+ # Questions: omalled@lanl.gov, ellenble@gmail.com
+
+import PCGA
+import Krigapping
+using JLD
+
+const RANDFLAG = 1
+const SAVEFLAG = 0  #switch to 1 to save data
+const LMFLAG = 0 #switch to false for vanilla PCGA, 1 means LM algo for GA
+#in Nowak and Cirpka 2004
+
+#Run the optimization loop until it converges or a total_iter number of times
+const total_iter = 10
+const pdrift = 1 #dimension of the drift matrix
+
+include("blob.jl") #get R, Q
+testForward = forwardObsPoints
+Gamma = R
+strue = [truelogk1[:]; truelogk2[:]] #vectorized 2D parameter field
+yvec = u_obsNoise # see ellen.jl for noise level
+
+const p = 20 # The oversampling parameter for increasing RSVD accuracy
+const q = 3 # Number of power iterations in the RSVD
+const K = 120 # Set the rank of the RSVD for Q, take i.e. K =
+    # ceil(length(strue)/7) 50413416850
+
+Z = PCGA.randSVDzetas(Q,K,p,q) 
+numparams = length(strue) 
+
+Zis = Array{Float64, 1}[Z[:,1],Z[:,2]];
+
+for i = 3:size(Z,2)
+    Zis = push!(Zis,Z[:,i])
+end
+
+mean_s = zeros(length(strue));
+s0 = mean_s;
+
+if RANDFLAG == 1
+    N  = size(R,2)
+    Kred = 2000
+	#Kred = 250
+    #Kred = 10
+    @show(Kred)
+    srand(1)
+    S_type = 1/sqrt(N)*randn(Kred,N)
+    typeS = "Gaussian"
+elseif RANDFLAG == 2
+    #put Achlioptas, rad here
+end
+
+
+if LMFLAG == 1
+    sbar,RMSE,cost,iterCt =  PCGA.pcgaiterationlm(testForward,s0,mean_s,Zis,Gamma,yvec,strue,
+      maxIter=total_iter,lmoption=LMFLAG)
+elseif RANDFLAG == 0
+    sbar,RMSE,cost,iterCt =  PCGA.pcgaiteration(testForward,s0,mean_s,Zis,Gamma,yvec,strue,
+                                                maxIter=total_iter)
+elseif RANDFLAG == 1
+    totaltime_PCGA = @elapsed sbar,RMSE,cost,iterCt =  PCGA.rgaiteration(testForward,s0,mean_s,Zis,Gamma,yvec, strue, S_type,
+                                            maxIter=total_iter,randls=true)
+    #@time sbar,RMSE,cost,iterCt =  PCGA.pcgaiteration(testForward,s0,mean_s,Zis,Gamma,yvec, strue,
+    #                                        maxIter=total_iter,randls=true,S=[S_type zeros(Kred, 1); zeros(1, N) 1.])
+	@show RMSE
+else
+    println("check LMFLAG,RANDFLAG")
+end
+
+#=
+rank_QK = rank(Z*Z')
+rmse_s_endminus1 = RMSE[iterCt-1]
+
+=#
+
+function plotresults(colorchoice)
+    nrow = 2
+    ncol = 4 
+
+    k1mean, k2mean = x2k(mean_s) #mean_s is all 0's for case 1, 0.3 for
+    #case 2
+    logk_mean = ks2k(k1mean,k2mean)
+
+    k1s0,k2s0 = x2k(s0)
+    logk_s0 = ks2k(k1s0,k2s0)
+
+    fig = figure(figsize=(6*ncol, 6*nrow)) 
+
+    vmin = minimum(logk)
+    vmax = maximum(logk)
+
+    plotfield(logk,nrow,ncol,1,vmin,vmax,noObs=true,mycmap=colorchoice)
+    grid(linewidth=3)    
+    title("the true logk")
+
+    plotfield(logk_mean,nrow,ncol,2,vmin,vmax,noObs=true,mycmap=colorchoice)
+    grid(linewidth=3)    
+    plt.title("the mean")
+
+    plotfield(logk_s0,nrow, ncol,3,vmin,vmax,noObs=true,mycmap=colorchoice)
+    grid(linewidth=3)
+    plt.title("s0")
+
+    #plotting the iterates
+    j=1
+    for i = [1:4,iterCt]
+        k1p_i,k2p_i = x2k(sbar[:,i+1]);
+        logkp_i = ks2k(k1p_i,k2p_i)
+        plotfield(logkp_i,nrow,ncol,3+j,vmin,vmax,noObs=true,mycmap=colorchoice)
+        grid(linewidth=3)       
+        plt.title("s_$(i)")
+        j=j+1
+    end
+
+
+    rmse_s_end = RMSE[iterCt]
+    rounderr =  round(rmse_s_end*10000)/10000
+
+    if RANDFLAG == 0
+
+        suptitle("PCGA 2D, noise=$(noise)%,its=$(iterCt),covdenom=$(covdenom),
+                 alpha=$(alpha),RMSE=$(rounderr),time=$(totaltime_PCGA)",fontsize=16)        
+
+    else
+
+        astr = typeS*" Randomized PCGA 2D, noise=$(noise)%,its=$(iterCt),covdenom=$(covdenom), alpha=$(alpha),RMSE=$(rounderr),time=$(totaltime_PCGA)"
+        suptitle(astr,fontsize=16)        
+    end
+
+    ax1 = axes([0.92,0.1,0.02,0.8])   
+    colorbar(cax = ax1)
+end
+
+plotresults("seismic")
+
+figure()
+plot(0:iterCt,RMSE[1:iterCt+1],linestyle="-",marker="o")
+title("2D RMSE vs iteration number, PCGA method, noise=$(noise)%")
+
+figure()
+plot(1:iterCt,cost[1:iterCt],linestyle="-",marker="o")
+title("2D cost vs iteration number, PCGA method,
+      noise=$(noise)%")
+
+
+if SAVEFLAG == 1
+    if RANDFLAG == 0
+        str="PCGAnoise$(noise)__al$(alpha)_cov$(covdenom)obs$(numobs).jld"
+    elseif RANDFLAG == 1
+        str="GRPCGAnoise$(noise)__al$(alpha)_cov$(covdenom)obs$(numobs)K$(Kred).jld"
+    end
+    @show(str)   
+    println("saving")
+    save(str,"sbar",sbar,"cost",cost,"totaltime_PCGA",totaltime_PCGA,"RMSE",RMSE,"iterCt",iterCt)
+elseif SAVEFLAG == 0
+    println("not saving")
+end
+
+@show(iterCt,rmse_s_endminus1, rmse_s_end, totaltime_PCGA,
+      rank_QK,p)
+
+if EXAMPLEFLAG == 2
+    @show(covdenom,alpha)
+end
+
+
+# References: 
+# Jonghyun Lee and Peter K. Kitanidis, 
+# Large-Scale Hydraulic Tomography and Joint Inversion of Head and
+# Tracer Data using the Principal Component Geostatistical Approach
+# (PCGA), 
+# Water Resources Research, 50(7): 5410-5427, 2014
+# Peter K. Kitanidis and Jonghyun Lee, 
+# Principal Component Geostatistical Approach for Large-Dimensional
+# Inverse Problem, 
+# Water Resources Research, 50(7): 5428-5443, 2014
